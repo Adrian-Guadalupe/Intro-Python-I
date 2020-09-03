e9a8ec882c216cb74b0b9ba4c9a2b1a80194da52
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-09-02 21:04:36 -0400

    5 files complete

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -8,14 +8,24 @@ changes, you'll get a TypeError saying you can't perform an operation
 on a string and an integer.
 """
 
+
+
+
+#%%
+# Write a print statement that combines x + y into the integer value 12
+# YOUR CODE HERE
+
 x = 5
 y = "7"
-
-# Write a print statement that combines x + y into the integer value 12
-
-# YOUR CODE HERE
+print(x + int(y))
 
 
+
+# %%
 # Write a print statement that combines x + y into the string value 57
-
 # YOUR CODE HERE
+
+x = 5
+y = "7"
+print(str(x) + y)
+# %%
--- src/04_printing.py
+++ src/04_printing.py
@@ -4,6 +4,7 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
+#%%
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -12,6 +13,19 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('x is %d, y is %f, z is "%s"' % (x, round(y, 2), z))
+############################################################################
+
+
+#%%
 # Use the 'format' string method to print the same thing
 
+print('x is {a}, y is {b}, z is {c}'.format(a = 10, b = 2.25, c = '"I like turtles!"'))
+############################################################################
+
+
+# %%
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {round(y, 2)}, z is "{z}"')
+############################################################################
+# %%
